a4d0861db94cdd3109e275baf71da98cc0f5c1f9
omi099 <76110622+omi099@users.noreply.github.com>
2025-01-19 17:43:36 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -21,15 +21,13 @@ AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
-userbot = Client("saverestricted", api_hash=API_HASH, api_id=API_ID) 
+userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
 
-# try:
-#     userbot.start()
-# except BaseException:
-#     print("Userbot Error ! Have you added SESSION while deploying??")
-#     sys.exit(1)
-
-userbot.start()
+try:
+    userbot.start()
+except BaseException:
+    print("Userbot Error ! Have you added SESSION while deploying??")
+    sys.exit(1)
 
 Bot = Client(
     "SaveRestricted",
